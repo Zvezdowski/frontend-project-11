65bad27b282ff59e04c536adbb19f35cee2da3ac
seshmonke <osk1sky@yandex.ru>
2024-04-08 14:34:26 +0300

    path test

--- src/render.js
+++ src/render.js
@@ -1,8 +1,9 @@
 import onChange from 'on-change';
 
 export default (state, elements) => {
+  console.log('troubleshooting');
   const watchedState = onChange(state, (path, value, previousValue) => {
     console.log(path, value, previousValue);
-    return watchedState;
   });
+  return watchedState;
 };
