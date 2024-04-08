6c1752564ba1c31075039a2a440172bac4ee8859
seshmonke <osk1sky@yandex.ru>
2024-04-08 16:46:45 +0300

    render dev

--- src/render.js
+++ src/render.js
@@ -2,6 +2,7 @@ import onChange from 'on-change';
 
 export default (state, elements) => {
   const watchedState = onChange(state, (path, value, previousValue) => {
+    console.log(path);
     switch (path) {
       case 'form.error':
         try {
